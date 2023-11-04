43c52ae7e8324a4126f0b08cd3d6fdf56db95e97
Spiewart <spiewart@gmail.com>
2023-11-04 10:00:52 -0500

    Initial commit.

---
+++ gouthelper/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from gouthelper.users.models import User
+from gouthelper.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
